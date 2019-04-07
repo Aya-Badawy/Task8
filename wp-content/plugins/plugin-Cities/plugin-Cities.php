362028d11584d9fef90ecaec25095a78cda27d53
ayabadawy <ayaahmed20az@gmail.com>
2019-04-07 13:04:51 +0200

    #4 [/plugin- Countries.php]

---
+++ wp-content/plugins/plugin-Cities/plugin-Cities.php
@@ -0,0 +1,20 @@
+<?php
+ /**
+ plugin Name: Cities.
+ Description: basic word press Plugin for custom Cities posts Type.
+ * Author:     hiker-onTrail.com
+ * License:     GPL2
+ *blog is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+{Cities} is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with blog.  
+ 
+ */
