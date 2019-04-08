e5fd1a0bf47a9a055523afb2c7af8f96852c8b56
ayabadawy <ayaahmed20az@gmail.com>
2019-04-08 14:30:39 +0200

    #12 [template-Cities.php] add the custom template to listing cities (add the style / header/footer/ script ) or templates (custom-posts-struct) folder

---
+++ wp-content/themes/oceanwp/custom-posts-Struct/header.php
@@ -0,0 +1,31 @@
+<?php /*Template Name: Cities*/ ?>
+<!DOCTYPE html>
+<html lang="en">
+<title>Cities</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css" type="text/css">
+
+<body>
+
+<!-- Navbar -->
+<div class="w3-top">
+  <div class="w3-bar w3-blue w3-card w3-left-align w3-large">
+    <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+    <a href="<?php echo get_site_url(); ?>" class="w3-bar-item w3-button w3-padding-large w3-white">Home</a>
+   
+  </div>
+
+  <!-- Navbar on small screens -->
+  <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
+    <a href="<?php echo get_site_url(); ?>" class="w3-bar-item w3-button w3-padding-large">Home</a>
+    
+  </div>
+</div>
+
+<!-- Header -->
